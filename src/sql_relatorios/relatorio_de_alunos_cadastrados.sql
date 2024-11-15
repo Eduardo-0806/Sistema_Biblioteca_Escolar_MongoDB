eefc43f8ab2876ec309d692f1b8d2f6ddd0aad3b
Eduardo-0806 <eduardoscota08@gmail.com>
2024-11-15 00:13:16 -0300

    Exclusão diretorio sql_relatorios
    Exclusão de diretorio sem utilidade no novo sistema, considerando o banco de dados e a forma que são realizadas as consultas

--- src/sql_relatorios/relatorio_de_alunos_cadastrados.sql
+++
@@ -1,4 +0,0 @@
-select matricula as 'Matricula do Aluno',
-	nome as 'Nome Aluno',
-    email as 'Email Aluno'
-    from ALUNOS order by nome;
--- src/sql_relatorios/relatorio_de_livros_acervos.sql
+++
@@ -1,8 +0,0 @@
-select id as 'ID',
-	nome_obra as 'Nome da Obra',
-    autor as 'Autor da Obra',
-    editora_edicao as 'Editora da Edicao',
-    numero_edicao as 'Numero da Edicao',
-    ano_edicao as 'Ano da Edicao',
-    quantidade_exemplares as 'Quantidade de Exemplares'
-    from LIVROS order by nome_obra, autor;
