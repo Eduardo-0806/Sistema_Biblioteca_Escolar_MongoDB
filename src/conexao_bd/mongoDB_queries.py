001d5d732ac372851fb84c3201cee87c3325e707
Eduardo-0806 <eduardoscota08@gmail.com>
2024-11-17 21:40:22 -0300

    Update mongoDB_queries.py

--- src/conexao_bd/mongoDB_queries.py
+++ src/conexao_bd/mongoDB_queries.py
@@ -1,6 +1,11 @@
 import pymongo
 
 class MongoDBQueries:
+    """
+    Classe MongoDBQueries - Responsavel por controlar a conexao com o banco de dados MongoDB, permitindo a realizacao
+    dos comandos NoSQL no sistema do projeto, manipulando suas colecoes e documentos
+    """
+
     def __init__(self):
         self.host = "localhost"
         self.port = 27017
@@ -14,9 +19,18 @@ class MongoDBQueries:
             self.close()
 
     def connect(self):
+        """
+        Metodo connect - Responsavel pela realizacao da conexao com o banco de dados, utilizando as informacoes de
+        usuario e senha para tal
+        """    
+
         self.mongo_client = pymongo.MongoClient(f"mongodb://{self.usuario}:{self.senha}@localhost:27017/")
         self.db = self.mongo_client["labdatabase"]
 
     def close(self):
+        """
+        Metodo close - Responsavel por fechar a conexao com o banco de dados MongoDB
+        """
+
         self.mongo_client.close()
     
